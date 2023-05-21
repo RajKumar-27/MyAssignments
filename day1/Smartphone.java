842609aa392525dd733d1bf2d6e40f1e9a883dc5
RajKumar-27 <134187616+RajKumar-27@users.noreply.github.com>
2023-05-21 03:01:32 -0600

    Add files via upload

---
+++ day1/Smartphone.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Iphone i1 = new Iphone();
+i1.makeCall();
+i1.model();
+	}
+
+}
