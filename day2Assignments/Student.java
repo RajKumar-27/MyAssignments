ff0d027703b2bf500d4b4d18f3d00010047e0978
RajKumar-27 <134187616+RajKumar-27@users.noreply.github.com>
2023-06-02 11:46:00 +0530

    Delete day2Assignments directory

--- day2Assignments/Student.java
+++
@@ -1,9 +0,0 @@
-package week1.day2Assignments;
-
-public class Student {
-String studentName="Raj Kumar";
-int rollNo = 10001;
-String collegeName = "Voorhees College";
-int marksScored = 546;
-int cgpa = 76;
-}
